0332e73482fc7cde7584963d4e8682c9ef91cb8e
Diego Gutiérrez <diegogutcat28@gmail.com>
2022-12-03 18:33:03 -0500

    Creado el componente para los archivos

--- src/components/FilesSideBar.tsx
+++ src/components/FilesSideBar.tsx
@@ -1,10 +1,20 @@
 import '@/styles/FilesSideBar.css';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Button from '@/components/Button';
+import File from '@/components/File';
 
 function FilesSideBar(): JSX.Element {
 	return (
 		<div className="FilesSideBar">
-			<Button className="main-txt">Add +</Button>
+			<Button className="main-txt">
+				Add <FontAwesomeIcon icon={faPlus} />
+			</Button>
+			<div className="files">
+				<File>index.js</File>
+				<File>exampleTest.js</File>
+				<File>sidebar.js</File>
+			</div>
 		</div>
 	);
 }
