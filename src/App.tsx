223744a048cb203c4413401fdd194984a1e6b14d
Diego Gutiérrez <diegogutcat28@gmail.com>
2022-12-04 21:37:51 -0500

    Se añadió el editor de código

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import '@/App.css';
 import Editor from '@/components/Editor';
 import FilesSideBar from '@/components/FilesSideBar';
 import Menu from '@/components/Menu';
+import CodeContext from '@/context/CodeContext';
+import useCode from '@/hooks/useCode';
 
 function App(): JSX.Element {
+	const code = useCode('');
+
 	return (
-		<div className="App">
-			<Menu />
-			<FilesSideBar />
-			<Editor />
-		</div>
+		<CodeContext.Provider value={code}>
+			<div className="App">
+				<Menu />
+				<FilesSideBar />
+				<Editor />
+			</div>
+		</CodeContext.Provider>
 	);
 }
 
--- src/components/EditorCode.tsx
+++ src/components/EditorCode.tsx
@@ -1,7 +1,40 @@
+import Editor, { useMonaco } from '@monaco-editor/react';
+import { useEffect, useState } from 'react';
 import '@/styles/EditorCode.css';
 
 function EditorCode(): JSX.Element {
-	return <div className="EditorCode" />;
+	const monaco = useMonaco();
+	const [code, setCode] = useState('');
+
+	useEffect(() => {
+		if (monaco !== null) {
+			monaco.editor.defineTheme('independence', {
+				base: 'vs-dark',
+				inherit: true,
+				rules: [],
+				colors: {
+					'editor.background': '#3a405a',
+					'editor.lineHighlightBackground': '#30344B',
+					'scrollbarSlider.background': '#282c3e',
+					'scrollbarSlider.hoverBackground': '#282c3e',
+					'scrollbarSlider.activeBackground': '#282c3e',
+				},
+			});
+		}
+	}, [monaco]);
+
+	return (
+		<div className="EditorCode">
+			{monaco !== null ? (
+				<Editor
+					height="100%"
+					theme="independence"
+					language="javascript"
+					value={code}
+				/>
+			) : null}
+		</div>
+	);
 }
 
 export default EditorCode;
