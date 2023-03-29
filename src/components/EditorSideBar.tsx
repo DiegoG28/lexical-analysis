3ac75f027951242238a46e98fbbe868685455f80
Diego Gutiérrez <diegogutcat28@gmail.com>
2023-03-29 12:45:42 -0500

    Se validó el stack de palabras clave

--- src/components/EditorSideBar.tsx
+++ src/components/EditorSideBar.tsx
@@ -15,31 +15,113 @@ function EditorSideBar(): JSX.Element {
 	const terminal = useTerminal();
 
 	const handleCode = (sourceCode: string | undefined): void => {
-		const start = 'elinicio';
-		const end = 'elfin';
+		const START_PROGRAM = 'elinicio';
+		const END_PROGRAM = 'elfin';
+		const START_VAR_ZONE = 'sietevar';
+		const END_VAR_ZONE = 'sietevarfin';
+		const START_BODY_ZONE = 'iniciobody';
+		const END_BODY_ZONE = 'finbody';
+
 		const errors: IError[] = [];
+		const stack: string[] = [];
 
 		if (!sourceCode) return;
 
 		const lines = sourceCode.split('\n');
-		const startLine = lines.findIndex(line => line.trim() === start);
-		const endLine = lines.findIndex(
-			(line, index) => index > startLine && line.trim() === end,
-		);
+		const startLine = lines.findIndex(line => line.trim() === START_PROGRAM);
 
 		if (startLine === -1) {
 			errors.push({
 				type: 'sintaxis',
-				message: `Se esperaba la palabra clave '${start}' al inicio del programa`,
+				message: `Se esperaba la palabra clave '${START_PROGRAM}' al inicio del programa`,
 				line: 1,
 			});
 		}
+		stack.push(START_PROGRAM);
 
-		if (endLine === -1) {
-			errors.push({
-				type: 'sintaxis',
-				message: `Se esperaba la palabra clave '${end}' al final del programa`,
-				line: lines.length,
+		let isVarSintaxCorrect = true;
+		let isBodySintaxCorrect = true;
+
+		for (let i = startLine + 1; i < lines.length; i++) {
+			const line = lines[i].trim();
+			console.log(stack);
+
+			if (line === START_VAR_ZONE) {
+				if (isVarSintaxCorrect) stack.push(START_VAR_ZONE);
+			} else if (line === END_VAR_ZONE) {
+				if (stack[stack.length - 1] !== START_VAR_ZONE) {
+					errors.push({
+						type: 'sintaxis',
+						message: `Se esperaba la palabra clave '${START_VAR_ZONE}' antes de '${END_VAR_ZONE}'`,
+						line: i + 1,
+					});
+					isVarSintaxCorrect = false;
+				} else {
+					stack.pop();
+					stack.push(END_VAR_ZONE);
+				}
+			} else if (line === START_BODY_ZONE) {
+				if (isVarSintaxCorrect) {
+					if (stack[stack.length - 1] !== END_VAR_ZONE) {
+						errors.push({
+							type: 'sintaxis',
+							message: `Se esperaba la palabra clave '${END_VAR_ZONE}' antes de '${START_BODY_ZONE}'`,
+							line: i + 1,
+						});
+						isBodySintaxCorrect = false;
+					} else if (isBodySintaxCorrect) {
+						stack.pop();
+						stack.push(START_BODY_ZONE);
+					}
+				} else {
+					const stackHasEndBody = stack.find(
+						keyword => keyword === END_BODY_ZONE,
+					);
+					if (stackHasEndBody) {
+						errors.push({
+							type: 'sintaxis',
+							message: `Se esperaba la palabra clave '${START_BODY_ZONE}' antes de '${END_BODY_ZONE}'`,
+							line: i + 1,
+						});
+						isBodySintaxCorrect = false;
+					} else if (isBodySintaxCorrect) {
+						stack.push(START_BODY_ZONE);
+					}
+				}
+			} else if (line === END_BODY_ZONE) {
+				if (
+					stack[stack.length - 1] !== START_BODY_ZONE &&
+					isBodySintaxCorrect
+				) {
+					errors.push({
+						type: 'sintaxis',
+						message: `Se esperaba la palabra clave '${START_BODY_ZONE}' antes de '${END_BODY_ZONE}'`,
+						line: i + 1,
+					});
+					isBodySintaxCorrect = false;
+				} else {
+					stack.pop();
+				}
+			} else if (line === END_PROGRAM) {
+				if (stack[stack.length - 1] !== START_PROGRAM && stack.length > 0) {
+					errors.push({
+						type: 'sintaxis',
+						message: `Se esperaba la palabra clave '${START_PROGRAM}' antes de '${END_PROGRAM}'`,
+						line: i + 1,
+					});
+				} else {
+					stack.pop();
+				}
+			}
+		}
+
+		if (stack.length > 0) {
+			stack.forEach(keyword => {
+				errors.push({
+					type: 'sintaxis',
+					message: `Se esperaba la palabra clave '${keyword}' correspondiente`,
+					line: lines.length,
+				});
 			});
 		}
 		terminal.addOutput(errors);
